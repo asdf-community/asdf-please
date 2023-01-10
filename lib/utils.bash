6a592832122edc5839c1c3d9d32c22215e1ba2c4
jtzero ? <jtzero511@gmail.com>
2023-01-10 11:57:29 -0500

    linting

--- lib/utils.bash
+++ lib/utils.bash
@@ -34,15 +34,15 @@ platform() {
   local platform
 
   case "$(uname -m)" in
-    x86_64 | x86-64 | x64 | amd64) architecture="amd64" ;;
-    arm64) architecture="arm64" ;;
-    *) fail "Unsupported architecture" ;;
+  x86_64 | x86-64 | x64 | amd64) architecture="amd64" ;;
+  arm64) architecture="arm64" ;;
+  *) fail "Unsupported architecture" ;;
   esac
 
   case "$(uname -s)" in
-    Darwin*) platform="darwin_${architecture}" ;;
-    Linux*) platform="linux_${architecture}" ;;
-    *) fail "Unsupported platform" ;;
+  Darwin*) platform="darwin_${architecture}" ;;
+  Linux*) platform="linux_${architecture}" ;;
+  *) fail "Unsupported platform" ;;
   esac
 
   echo "$platform"
