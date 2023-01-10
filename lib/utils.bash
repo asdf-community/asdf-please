8bf5cccdac4c5f8247c44dd4870621401a038aab
jtzero ? <jtzero511@gmail.com>
2023-01-10 12:10:58 -0500

    improve listing

--- lib/utils.bash
+++ lib/utils.bash
@@ -3,6 +3,8 @@
 set -eo pipefail
 
 GH_REPO="https://github.com/thought-machine/please"
+TOOL_NAME="please"
+TOOL_TEST="please -v"
 
 fail() {
   echo -e "\e[31mFail:\e[m $*"
@@ -16,16 +18,22 @@ if test -n "$GITHUB_API_TOKEN"; then
 fi
 
 sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+  sort -V
 }
 
 list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" | grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//'
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
+  list_github_tags
+}
+
+list_all_versions() {
+  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
+  # Change this function if <YOUR TOOL> has other means of determining installable versions.
   list_github_tags
 }
 
